786de72f0c271b32fb3b9ba3773eca0bf53bc155
veedahv <veedahv55@gmail.com>
2021-03-31 17:36:31 +0100

    new commit

--- js/budget.js
+++ js/budget.js
@@ -1,5 +1,4 @@
 
-
 const form = document.querySelector('form'),
     inputItem = document.querySelector('#item'),
     inputBudget = document.getElementById('budget'),
@@ -8,10 +7,16 @@ const form = document.querySelector('form'),
     currency = document.createElement('span'),
     table = document.querySelector('table'),
     tableBody = document.querySelector('tbody'),
+    itemSortAsc = document.querySelector('.itemSortAsc'),
+    itemSortDesc = document.querySelector('.itemSortDesc'),
+    priceSortAsc = document.querySelector('.priceSortAsc'),
+    priceSortDesc = document.querySelector('.priceSortDesc'),
+    selectCurrency = document.getElementById('select-currency'),
     clearBtn = document.getElementById('clear'),
     savePdf = document.getElementById('savePdf'),
     emailShare = document.getElementById('emailShare'),
     whatsAppShare = document.getElementById('whatsAppShare'),
+    whatsAppLinkTag = document.getElementById('whatsapp-link'),
     totalInput = document.getElementById('total'),
     totalItems = document.getElementById('total-items'),
     totalDiv = document.querySelector('.total-div-contain'),
@@ -23,13 +28,29 @@ const form = document.querySelector('form'),
 
 
 let budgetArr = [],
- t = 0,
+    itemArr = [
+        {
+            objName: 'House',
+            objPrice: 10000,
+        },
+        {
+            objName: 'Pork',
+            objPrice: 550,
+        },
+        {
+            objName: 'Fish',
+            objPrice: 700,
+        },
+        {
+            objName: 'Mat',
+            objPrice: 1000,
+        }
+    ],
+    t = 0,
     i = 0,
     sN = 1;
+
 currency.innerHTML = '&#8358;';
-// currency.innerHTML = '&#36;';
-// currency.innerHTML = '&#euro;';
-// currency.innerHTML = '&#163;';
 
 // currency.innerHTML = '&#65505;';
 
@@ -40,9 +61,47 @@ if (savedBudgetArr) {
     })
     localStorage.setItem('budgetItemsArr', JSON.stringify(budgetArr));
 }
+const sortTable = () => {
+    while (tableBody.hasChildNodes()) {
+        tableBody.removeChild(tableBody.childNodes[0]);
+    }
+    t = 0;
+    // itemArr.forEach((arrItem) => {
+    //     name = arrItem.objName;
+    //     price = arrItem.objPrice;
+    //     createNewTableRow(name, price);
+    // })
+    budgetArr.forEach((arrItem) => {
+        name = arrItem.objName;
+        price = arrItem.objPrice;
+        createNewTableRow(name, price);
+    })
+}
+selectCurrency.addEventListener('change', (event) => {
+    switch (event.target.value) {
+        case '1':
+            currency.innerHTML = '&#8358;';
+            sortTable();
+            break;
+        case '2':
+            currency.innerHTML = '&#36;';
+            sortTable();
+            break;
+        case '3':
+            currency.innerHTML = '&euro;';
+            sortTable();
+            break;
+        case '4':
+            currency.innerHTML = '&#163;';
+            sortTable();
+            break;
+        default:
+            break;
+    }
+})
 //change font
 fontChange.addEventListener('click', (event) => {
-(document.body.style[0] === '--font-family') ? document.body.removeAttribute('style') : document.body.setAttribute('style', "--font-family: 'Tillana', cursive");
+    (document.body.style[0] === '--font-family') ? document.body.removeAttribute('style') : document.body.setAttribute('style', "--font-family: 'Tillana', cursive");
 })
 // change to default mode
 modeBtns.forEach(function (btn) {
@@ -114,11 +173,11 @@ const createNewTableRow = (name, price) => {
             totalDiv.style.display = 'flex';
         }
         totalItems.value = sN;
-        
+
     }
     let i = 0;
     function editRow(edit) {
-        
+
         let editIcon = edit.querySelector('i');
         let editTableRow = edit.closest('.table-row');
         let editCellName = editTableRow.querySelector('.item-name');
@@ -162,7 +221,7 @@ const createNewTableRow = (name, price) => {
             if (Array.from(deleteTableRow.parentNode.children).indexOf(deleteTableRow) === budgetArr.indexOf(budgetArrItem)) {
                 budgetArr.splice(Array.from(deleteTableRow.parentNode.children).indexOf(deleteTableRow), 1);
                 localStorage.setItem('budgetItemsArr', JSON.stringify(budgetArr))
-            } 
+            }
         })
         deleteTableRow.remove();
         t = t - parseInt(price);
@@ -193,12 +252,74 @@ const createNewTableRow = (name, price) => {
 budgetArr.forEach((budgetArrItem) => {
     name = budgetArrItem.objName;
     price = budgetArrItem.objPrice;
+    // itemArr.push(budgetArrItem);
     createNewTableRow(name, price);
+    localStorage.setItem('budgetItemsArr', JSON.stringify(budgetArr));
+})
+// itemArr.forEach((arrayItem) => {
+//     name = arrayItem.objName;
+//     price = arrayItem.objPrice;
+//     createNewTableRow(name, price);
+// })
+const compareItemAsc = (a, b) => {
+    // Use toUpperCase() to ignore character casing
+    const itemA = a.objName.toUpperCase();
+    const itemB = b.objName.toUpperCase();
+
+    if (itemA > itemB) {
+        return 1;
+    } else if (itemA < itemB) {
+        return -1;
+    } else {
+        return 0;
+    }
+    // return comparison;
+}
+const compareItemDesc = (a, b) => {
+    // Use toUpperCase() to ignore character casing
+    const itemA = a.objName.toUpperCase();
+    const itemB = b.objName.toUpperCase();
+
+    let comparison = 0;
+    if (itemA > itemB) {
+        comparison = 1;
+    } else if (itemA < itemB) {
+        comparison = -1;
+    }
+    return comparison * -1;
+}
+
+const comparePriceAsc = (a, b) => {
+    return a.objPrice - b.objPrice;
+}
+const comparePriceDesc = (a, b) => {
+    return b.objPrice - a.objPrice;
+}
+
+itemSortAsc.addEventListener('click', (event) => {
+    event.preventDefault();
+    budgetArr.sort(compareItemAsc);
+    sortTable();
+})
+itemSortDesc.addEventListener('click', (event) => {
+    event.preventDefault();
+    budgetArr.sort(compareItemDesc);
+    sortTable();
+})
+priceSortAsc.addEventListener('click', (event) => {
+    event.preventDefault();
+    budgetArr.sort(comparePriceAsc);
+    sortTable();
+})
+priceSortDesc.addEventListener('click', (event) => {
+    event.preventDefault();
+    budgetArr.sort(comparePriceDesc);
+    sortTable();
 })
 
 const newTableRow = () => {
     let price = inputBudget.value,
-    y = inputItem.value,
+        y = inputItem.value,
         firstLetter = y[0].toUpperCase(),
         smallLetters = y.slice(1),
         name = firstLetter + smallLetters;
@@ -208,27 +329,43 @@ const newTableRow = () => {
         objPrice: price,
     }
     budgetArr.push(budgetObj);
+    itemArr.push(budgetObj);
     localStorage.setItem('budgetItemsArr', JSON.stringify(budgetArr))
 
 
 }
 const printDone = () => {
     document.querySelectorAll('.printNot').forEach(printNotElement => {
-        printNotElement.classList.remove('print-not-class');      
-    });    
+        printNotElement.classList.remove('print-not-class');
+    });
 }
 const printFunc = () => {
     document.querySelectorAll('.printNot').forEach(printNotElement => {
-        printNotElement.classList.add('print-not-class');        
+        printNotElement.classList.add('print-not-class');
     });
     window.print();
-setTimeout(printDone, 2000);
+    setTimeout(printDone, 2000);
 }
-// printFunc();
+const txtFunc = () => {
+    let msgTxt = ``;
+    let itemSn = 1;
+
+
+    budgetArr.forEach((budgetArrItem) => {
+        name = itemSn + '. ' + budgetArrItem.objName;
+        price = currency.innerHTML + budgetArrItem.objPrice;        
+        msgTxt += `%0a${name} - ${price}`
+        itemSn++
+    })
+    price = currency.innerHTML + totalInput.value;
+    msgTxt += `%0aTotal - ${price}`
+    let whatsAppLink = `https://wa.me/?text=${msgTxt}`;
+    whatsAppLinkTag.href = whatsAppLink;
+}
 
 savePdf.addEventListener('click', printFunc);
 emailShare.addEventListener('click', printFunc);
-whatsAppShare.addEventListener('click', printFunc);
+whatsAppShare.addEventListener('click', txtFunc);
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     newTableRow();
